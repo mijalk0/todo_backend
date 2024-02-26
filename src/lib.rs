a195cf4d2250093fda2d675ff489c5ed245d0ad8
Luka Mijalkovic <luka.mijalkovic3@gmail.com>
2024-02-26 14:46:27 -0500

    Added query paramter and header extractors.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use axum::{
-    extract::Path,
+    extract::{Path, Query},
+    http::{header::USER_AGENT, HeaderMap, HeaderName, HeaderValue},
     routing::{get, post},
     Json, Router,
 };
@@ -11,7 +12,10 @@ pub async fn run() {
         .route("/", get(hello_world))
         .route("/extract_body", post(extract_body))
         .route("/extract_json", post(extract_json))
-        .route("/extract_path_variable/:id", get(extract_path_variable));
+        .route("/extract_path_variable/:id", get(extract_path_variable))
+        .route("/extract_query_params", get(extract_query_params))
+        .route("/extract_standard_header", get(extract_standard_header))
+        .route("/extract_custom_header", get(extract_custom_header));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -48,4 +52,34 @@ async fn extract_json(Json(json): Json<JsonRequest>) -> Json<JsonResponse> {
 
 async fn extract_path_variable(Path(id): Path<i32>) -> String {
     id.to_string()
+}
+
+#[derive(Serialize, Deserialize)]
+struct QueryParams {
+    message: String,
+    id: i32,
+}
+
+async fn extract_query_params(Query(query): Query<QueryParams>) -> Json<QueryParams> {
+    Json(query)
+}
+
+async fn extract_standard_header(headers: HeaderMap) -> String {
+    headers
+        .get(USER_AGENT)
+        .map(HeaderValue::to_str)
+        .unwrap_or(Ok(""))
+        .unwrap()
+        .into()
+}
+
+const CUSTOM_HEADER: HeaderName = HeaderName::from_static("x-custom-header");
+
+async fn extract_custom_header(headers: HeaderMap) -> String {
+    headers
+        .get(CUSTOM_HEADER)
+        .map(HeaderValue::to_str)
+        .unwrap_or(Ok(""))
+        .unwrap()
+        .into()
 }
